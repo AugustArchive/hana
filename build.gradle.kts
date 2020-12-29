cc9e5696fa2e8356b40bb193a776b1e486606349
Noel <august@augu.dev>
2020-12-28 18:19:11 -0700

    v3.0.0-indev.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ plugins {
     application
 }
 
-val ver = Version(2, 4, 1, VersionCandidate.RELEASED)
+val ver = Version(3, 0, 0, VersionCandidate.INDEV)
 
 group = "dev.floofy"
 version = ver.string()
