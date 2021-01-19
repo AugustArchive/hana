5e9b6b487fb5ea53c85177591c9350a53a96f64d
Noel <august@augu.dev>
2021-01-18 20:02:04 -0700

    Change license to 2020-2021 (I am 18 days late, yes)

--- src/main/kotlin/dev/floofy/api/endpoints/v2/MainEndpoint.kt
+++ src/main/kotlin/dev/floofy/api/endpoints/v2/MainEndpoint.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 August
+ * Copyright (c) 2020-2021 August
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package dev.floofy.api.endpoints.v2
 
 import dev.floofy.api.core.Endpoint
